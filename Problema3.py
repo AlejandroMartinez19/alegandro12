efffd83f45a3f2d61156a244eb44442a7bbc5e8b
AlejandroMartinez19 <alejandro.martinez19054@gmail.com>
2023-07-03 09:45:08 -0500

    agregar archivos al repositorio

---
+++ Problema3.py
@@ -0,0 +1,11 @@
+A = float(input("Leer A: "))
+B = float(input("Leer B: "))
+C = float(input("Leer C: "))
+
+D = (A + B + C)/ 3
+
+print("Imprimir: ", D)
+
+DR = round(D,2)
+
+print ("Imprimir: ", DR)
